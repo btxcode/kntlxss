8fbc31a6485a7642cded026e1d8e7d00fea26e0d
btxcode <kangian4@gmail.com>
2024-09-11 17:37:02 +0700

    Update kntlxss.sh

--- kntlxss.sh
+++ kntlxss.sh
@@ -30,23 +30,42 @@ install_tools() {
     echo "[*] Installing required tools..."
     sudo -v || { echo "[ERROR] Sudo required. Please run with sudo privileges."; exit 1; }
 
+    # Install Chrome
+    sudo apt update && sudo apt install libu2f-udev golang-go -y
+    sudo dpkg -i google-chrome-stable_114.0.5735.90-1_amd64.deb
+
+    # Copy chromedriver and set execute permissions
+    sudo chmod +x chromedriver
+    sudo cp chromedriver /usr/local/bin/
+
     # Install gf tool (Gf Patterns)
     go install github.com/tomnomnom/gf@latest
     sudo mv ~/go/bin/gf /usr/local/bin/
+    sudo chmod +x /usr/local/bin/gf
+
+    # Remove existing .gf directory if it exists and clone the new one
+    if [ -d "~/.gf" ]; then
+        echo "[*] Removing existing .gf directory..."
+        rm -rf ~/.gf
+    fi
+
+    echo "[*] Cloning .gf patterns..."
     git clone https://github.com/PushkraJ99/.gf
     mv .gf ~/
 
     # Install Arjun (HTTP parameter discovery tool) & hakrawler
     sudo apt update
-    sudo apt install arjun hakrawler
+    sudo apt install arjun hakrawler -y
 
     # Install waybackurls
     go install github.com/tomnomnom/waybackurls@latest
     sudo mv ~/go/bin/waybackurls /usr/local/bin/
+    sudo chmod +x /usr/local/bin/waybackurls
 
     # Install gau
     go install github.com/lc/gau/v2/cmd/gau@latest
     sudo mv ~/go/bin/gau /usr/local/bin/
+    sudo chmod +x /usr/local/bin/gau
 
     # Install waymore
     sudo pip3 install git+https://github.com/xnl-h4ck3r/waymore.git -v
@@ -55,6 +74,7 @@ install_tools() {
     # Install katana
     go install github.com/projectdiscovery/katana/cmd/katana@latest
     sudo mv ~/go/bin/katana /usr/local/bin/
+    sudo chmod +x /usr/local/bin/katana
 
     # Install Subdominator
     sudo pip3 install git+https://github.com/RevoltSecurities/Subdominator
@@ -65,16 +85,10 @@ install_tools() {
     # Install uro
     sudo pip3 install uro
 
-    # Install Chrome
-    sudo apt install libu2f-udev
-    sudo chmod +x chromedriver *.py *.sh *.txt *.deb
-    sudo cp chromedriver /usr/local/bin/
-    sudo dpkg -i google-chrome*.deb
-    mkdir output
-
-
     echo "[*] Tools installed successfully."
 }
+
+#!/bin/bash
 
 # Function to prompt for domain input and proceed with domain enumeration and crawling
 prompt_domain_and_proceed() {
@@ -107,27 +121,25 @@ crawl_and_filter_urls() {
     echo "[*] Crawling URLs..."
     
     # Crawl and count total URLs, saving output to domain-specific folder
-    waybackurls http://$domain | tee -a output/$domain/1.txt
-    gau http://$domain | tee -a output/$domain/2.txt
-    waymore -i http://$domain -mode U -oU output/$domain/3.txt
-    katana -u http://$domain -kf 3 | tee -a output/$domain/4.txt
+    waybackurls http://$domain | tee -a output/$domain/wayback.txt
+    gau http://$domain | tee -a output/$domain/gau.txt
+    waymore -i http://$domain -mode U -oU output/$domain/waymore.txt
+    katana -u http://$domain -kf 3 | tee -a output/$domain/katana.txt
 
     # Merging all results into one file in the domain-specific folder
-    cat output/$domain/*.txt > output/$domain/output.txt
-    total=$(wc -l < output/$domain/output.txt)
+    cat output/$domain/*.txt > output/$domain/combined_urls.txt
+    total=$(wc -l < output/$domain/combined_urls.txt)
 
     echo "[*] Merging and filtering URLs..."
-    cat output/$domain/output.txt | uniq > output/$domain/unique_urls1.txt
-    rm output/$domain/1.txt output/$domain/2.txt output/$domain/3.txt output/$domain/4.txt output/$domain/output.txt
+    cat output/$domain/combined_urls.txt | uniq | sort -u > output/$domain/unique_urls.txt
 
     # Hakrawler & Subprober filtering
     echo "[*] Running hakrawler and Subprober for domain filtering..."
     cat output/$domain/domains.txt | httpx-toolkit | hakrawler | tee -a output/$domain/links.txt
-    subprober -f output/$domain/domains.txt -sc -ar -o output/$domain/200-codes-urls.txt -nc -mc 200 -c 30
+    subprober -f output/$domain/domains.txt -sc -ar -o output/$domain/subprober_urls.txt -nc -mc 200 -c 30
 
-    # Merging unique_urls1.txt, links.txt, and 200-codes-urls.txt into urls.txt in the domain-specific folder
-    cat output/$domain/unique_urls1.txt output/$domain/links.txt output/$domain/200-codes-urls.txt > output/$domain/urls.txt
-    rm output/$domain/unique_urls1.txt output/$domain/links.txt output/$domain/200-codes-urls.txt
+    # Merging unique_urls.txt, links.txt, and subprober_urls.txt into all_urls.txt in the domain-specific folder
+    cat "output/$domain/unique_urls.txt" "output/$domain/links.txt" "output/$domain/subprober_urls.txt" > output/$domain/all_urls.txt
 
     # Continue with filtering
     initial_filtering
@@ -138,16 +150,15 @@ initial_filtering() {
     echo "[*] Performing initial filtering on URLs..."
     
     # Perform initial filtering and save results in the domain-specific folder
-    cat output/$domain/urls.txt | grep -E -v '\.css$|\.js$|\.jpg$|\.JPG$|\.PNG$|\.GIF$|\.avi$|\.dll$|\.pl$|\.webm$|\.c$|\.py$|\.bat$|\.tar$|\.swp$|\.tmp$|\.sh$|\.deb$|\.exe$|\.zip$|\.mpeg$|\.mpg$|\.flv$|\.wmv$|\.wma$|\.aac$|\.m4a$|\.ogg$|\.mp4$|\.mp3$|\.bat$|\.dat$|\.cfg$|\.cfm$|\.bin$|\.jpeg$|\.JPEG$|\.ps.gz$|\.gz$|\.gif$|\.tif$|\.tiff$|\.csv$|\.png$|\.ttf$|\.ppt$|\.pptx$|\.ppsx$|\.doc$|\.woff$|\.xlsx$|\.xls$|\.mpp$|\.mdb$|\.json$|\.woff2$|\.icon$|\.pdf$|\.docx$|\.svg$|\.txt$|\.jar$|\.0$|\.1$|\.2$|\.3$|\.4$|\.m4r$|\.kml$|\.pro$|\.yao$|\.gcn3$|\.PDF$|\.egy$|\.par$|\.lin$|\.yht$' > output/$domain/filtered_links.txt
-    grep -E '^https?://' output/$domain/filtered_links.txt | sed 's/\[200\]//g' > output/$domain/filtered_links2.txt
+    cat output/$domain/all_urls.txt | grep -E -v '\.css$|\.js$|\.jpg$|\.JPG$|\.PNG$|\.GIF$|\.avi$|\.dll$|\.pl$|\.webm$|\.c$|\.py$|\.bat$|\.tar$|\.swp$|\.tmp$|\.sh$|\.deb$|\.exe$|\.zip$|\.mpeg$|\.mpg$|\.flv$|\.wmv$|\.wma$|\.aac$|\.m4a$|\.ogg$|\.mp4$|\.mp3$|\.bat$|\.dat$|\.cfg$|\.cfm$|\.bin$|\.jpeg$|\.JPEG$|\.ps.gz$|\.gz$|\.gif$|\.tif$|\.tiff$|\.csv$|\.png$|\.ttf$|\.ppt$|\.pptx$|\.ppsx$|\.doc$|\.woff$|\.xlsx$|\.xls$|\.mpp$|\.mdb$|\.json$|\.woff2$|\.icon$|\.pdf$|\.docx$|\.svg$|\.txt$|\.jar$|\.0$|\.1$|\.2$|\.3$|\.4$|\.m4r$|\.kml$|\.pro$|\.yao$|\.gcn3$|\.PDF$|\.egy$|\.par$|\.lin$|\.yht$' > output/$domain/filtered_urls.txt
+    grep -E '^https?://' output/$domain/filtered_urls.txt | sed 's/\[200\]//g' > output/$domain/filtered_cleaned_urls.txt
 
     # Running uro for further filtering
     echo "[*] Running uro for further filtering..."
-    cat output/$domain/filtered_links2.txt | uro -b css js jpg JPG PNG GIF avi dll pl webm c py bat tar swp tmp sh deb exe zip mpeg mpg flv wmv wma aac m4a ogg mp4 mp3 bat dat cfg cfm bin jpeg JPEG ps.gz gz gif tif tiff csv png ttf ppt pptx ppsx doc woff xlsx xls mpp mdb json woff2 icon pdf docx svg txt jar 0 1 2 3 4 m4r kml pro yao gcn3 PDF egy par lin yht | tee -a output/$domain/lista.txt
+    cat output/$domain/filtered_cleaned_urls.txt | uro -b css js jpg JPG PNG GIF avi dll pl webm c py bat tar swp tmp sh deb exe zip mpeg mpg flv wmv wma aac m4a ogg mp4 mp3 bat dat cfg cfm bin jpeg JPEG ps.gz gz gif tif tiff csv png ttf ppt pptx ppsx doc woff xlsx xls mpp mdb json woff2 icon pdf docx svg txt jar 0 1 2 3 4 m4r kml pro yao gcn3 PDF egy par lin yht | tee -a output/$domain/uro_filtered.txt
 
     # Unique filtering and cleaning up temporary files
-    cat output/$domain/lista.txt | uniq > output/$domain/unique_urls2.txt
-    rm output/$domain/filtered_links*.txt output/$domain/lista.txt output/$domain/urls.txt
+    cat output/$domain/uro_filtered.txt | uniq | sort -u > output/$domain/final_filtered_urls.txt
 
     # Continue to final filtering
     final_filtering
@@ -158,16 +169,18 @@ final_filtering() {
     echo "[*] Performing final filtering..."
 
     # Save final filtered results in the domain-specific folder
-    grep '=' output/$domain/unique_urls2.txt > output/$domain/filtered_urls_with_params.txt   # Query URLs
-    grep -v '=' output/$domain/unique_urls2.txt > output/$domain/filtered_urls_without_params.txt  # Path URLs
-    cat output/$domain/unique_urls2.txt | grep -E "\.php|\.asp|\.aspx|\.cfm|\.jsp" | sort > output/$domain/output.txt
-    grep -v "http[^ ]*\.[^/]*\." output/$domain/unique_urls2.txt | grep "http" | sort > output/$domain/clean_urls.txt
+    grep '=' output/$domain/final_filtered_urls.txt > output/$domain/filtered_urls_with_params.txt   # Query URLs
+    grep -v '=' output/$domain/final_filtered_urls.txt > output/$domain/filtered_urls_without_params.txt  # Path URLs
+    cat output/$domain/final_filtered_urls.txt | grep -E "\.php|\.asp|\.aspx|\.cfm|\.jsp" | sort > output/$domain/output_php_asp.txt
+    grep -v "http[^ ]*\.[^/]*\." output/$domain/final_filtered_urls.txt | grep "http" | sort > output/$domain/clean_urls.txt
 
     # Running arjun for parameter discovery
-    arjun -i output/$domain/output.txt -w parameters.txt -t 1 -oT output/$domain/arjun_final.txt
+    arjun -i output/$domain/output_php_asp.txt -w parameters.txt -t 1 -oT output/$domain/arjun_params.txt
 
     # Merging all results into final_temp.txt in the domain-specific folder
-    cat output/$domain/filtered_urls_with_params.txt output/$domain/filtered_urls_without_params.txt output/$domain/output.txt output/$domain/clean_urls.txt output/$domain/arjun_final.txt > output/$domain/final_temp.txt
+    # cat output/$domain/filtered_urls_with_params.txt output/$domain/filtered_urls_without_params.txt output/$domain/output_php_asp.txt output/$domain/clean_urls.txt output/$domain/arjun_params.txt > output/$domain/final_urls.txt
+    cat "output/$domain/filtered_urls_with_params.txt" "output/$domain/filtered_urls_without_params.txt" "output/$domain/output_php_asp.txt" "output/$domain/clean_urls.txt" "output/$domain/arjun_params.txt" > "output/$domain/final_urls.txt"
+
 
     # Clean up intermediate files and proceed to the next steps
     cleanup_intermediate_files
@@ -176,37 +189,36 @@ final_filtering() {
 # Function to clean up intermediate files
 cleanup_intermediate_files() {
     # Further processing and filtering based on gf patterns
-    cat output/$domain/final_temp.txt | gf xss | sed 's/=.*/=/' | sed 's/URL: //' | uniq | sort -u > output/$domain/$domain-1.txt
-    cat output/$domain/final_temp.txt | gf sqli | sed 's/=.*/=/' | sed 's/URL: //' | uniq | sort -u > output/$domain/$domain-2.txt
-    cat output/$domain/final_temp.txt | gf ssrf | sed 's/=.*/=/' | sed 's/URL: //' | uniq | sort -u > output/$domain/$domain-3.txt
-    cat output/$domain/final_temp.txt | gf ssti | sed 's/=.*/=/' | sed 's/URL: //' | uniq | sort -u > output/$domain/$domain-4.txt
-    cat output/$domain/final_temp.txt | gf urlparams | sed 's/=.*/=/' | sed 's/URL: //' | uniq | sort -u > output/$domain/$domain-5.txt
-    cat output/$domain/final_temp.txt | gf redirect | sed 's/=.*/=/' | sed 's/URL: //' | uniq | sort -u > output/$domain/$domain-6.txt
-    cat output/$domain/final_temp.txt | gf idor | sed 's/=.*/=/' | sed 's/URL: //' | uniq | sort -u > output/$domain/$domain-7.txt
-    cat output/$domain/final_temp.txt | gf lfi | sed 's/=.*/=/' | sed 's/URL: //' | uniq | sort -u > output/$domain/$domain-8.txt
+    cat output/$domain/final_urls.txt | gf xss | sed 's/=.*/=/' | sed 's/URL: //' | uniq | sort -u > output/$domain/xss.txt
+    cat output/$domain/final_urls.txt | gf sqli | sed 's/=.*/=/' | sed 's/URL: //' | uniq | sort -u > output/$domain/sqli.txt
+    cat output/$domain/final_urls.txt | gf ssrf | sed 's/=.*/=/' | sed 's/URL: //' | uniq | sort -u > output/$domain/ssrf.txt
+    cat output/$domain/final_urls.txt | gf ssti | sed 's/=.*/=/' | sed 's/URL: //' | uniq | sort -u > output/$domain/ssti.txt
+    cat output/$domain/final_urls.txt | gf urlparams | sed 's/=.*/=/' | sed 's/URL: //' | uniq | sort -u > output/$domain/urlparams.txt
+    cat output/$domain/final_urls.txt | gf redirect | sed 's/=.*/=/' | sed 's/URL: //' | uniq | sort -u > output/$domain/redirect.txt
+    cat output/$domain/final_urls.txt | gf idor | sed 's/=.*/=/' | sed 's/URL: //' | uniq | sort -u > output/$domain/idor.txt
+    cat output/$domain/final_urls.txt | gf lfi | sed 's/=.*/=/' | sed 's/URL: //' | uniq | sort -u > output/$domain/lfi.txt
+
     # Final merging and cleanup
-    cat output/$domain/$domain-*.txt | uniq > output/$domain/final_temp2.txt
-    rm output/$domain/final_temp.txt output/$domain/$domain-*.txt
-
-    # running second tools
-    subfinder -u $domain -all -silent | httpx-toolkit > output/$domain/hasilsub.txt
-    cat output/$domain/hasilsub.txt | uniq | sort -u > output/$domain/finalsub.txt
-    bash xsscrawler.sh -l output/$domain/finalsub.txt -o output/$domain/final_temp3.txt
-    cat final_temp*.txt | uniq > final.txt
-    rm output/$domain/final_temp*.txt output/$domain/hasilsub.txt output/$domain/domains.txt
-
+    # cat output/$domain/xss.txt output/$domain/sqli.txt output/$domain/ssrf.txt output/$domain/ssti.txt output/$domain/urlparams.txt output/$domain/redirect.txt output/$domain/idor.txt output/$domain/lfi.txt | uniq > output/$domain/final.txt
+    cat "output/$domain/xss.txt" "output/$domain/sqli.txt" "output/$domain/ssrf.txt" "output/$domain/ssti.txt" "output/$domain/urlparams.txt" "output/$domain/redirect.txt" "output/$domain/idor.txt" "output/$domain/lfi.txt" | uniq > "output/$domain/final.txt"
+    # Menghilangkan port dari URL (port 80 dan 443, sebagai contoh umum)
+    sed -e 's/:80//g' -e 's/:443//g' "output/$domain/final.txt" | sort -u > "output/$domain/final_clean.txt"
+    # Clean up all intermediate files but keep final.txt and domains.txt
+    find output/$domain/ -type f ! -name 'domains.txt' ! -name 'final_clean.txt' ! -name 'final.txt' -delete
 
     # Run XSS scanners
     run_xss_scanners
 }
 
-# Function to run XSS scanners
+# Function to run XSS scanners (reflection and stored)
 run_xss_scanners() {
-    echo "[*] Running reflection XSS scanner..."
-    python3 reflection.py -l output/$domain/final.txt --threads 1 --rua
+    echo "[*] Running XSS scanners..."
 
-    echo "[*] Running stored XSS scanner..."
-    python3 stored.py -l output/$domain/final.txt --threads 1 --rua
+    # Run reflection XSS scanner
+    python3 reflection.py -l output/$domain/final_clean.txt --threads 1 --rua
+
+    # Run stored XSS scanner
+    python3 stored.py -l output/$domain/final_clean.txt --threads 1 --rua
 }
 
 # Quit function to clear terminal
